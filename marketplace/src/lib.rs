1de2d5b893546f7c77c903d9bff6e487132123b7
Myom_tech <iorchirmyom@gmail.com>
2026-08-29 17:24:35 +0100

    fix: lint: no missing_docs warning, public API drifts (#143)

--- marketplace/src/lib.rs
+++ marketplace/src/lib.rs
@@ -6,6 +6,7 @@
 
 #![no_std]
 #![allow(clippy::too_many_arguments)]
+#![warn(missing_docs)]
 
 use soroban_sdk::{
     contract, contracterror, contractimpl, contracttype, symbol_short, Address, Env, InvokeError,
