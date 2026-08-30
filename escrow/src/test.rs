150e0838ae73f014da73622f4db4e8d5ea4f8038
Ukorstack <Bornbless7501@gmail.com>
2026-08-30 12:12:10 +0000

    fix: correct constructor assertions

--- escrow/src/test.rs
+++ escrow/src/test.rs
@@ -43,7 +43,7 @@ mod test {
         let max_amount = 10000i128;
 
         let res = client.initialize(&admin, &fee_bps, &treasury, &min_amount, &max_amount);
-        assert!(res);
+        assert_eq!(res, ());
 
         let res_try = client.try_initialize(&admin, &fee_bps, &treasury, &min_amount, &max_amount);
         assert_eq!(res_try, Err(Ok(EscrowError::AlreadyInitialized)));
@@ -78,7 +78,7 @@ mod test {
 
         // Call constructor
         let res = client.constructor(&config);
-        assert!(res);
+        assert_eq!(res, ());
 
         // Verify admin is set correctly
         let admin_view = client.get_admin();
@@ -113,7 +113,7 @@ mod test {
 
         // Initialize via constructor
         let res = client.constructor(&config);
-        assert!(res);
+        assert_eq!(res, ());
 
         // Attempt to call initialize after constructor should fail
         let res_try = client.try_initialize(&admin, &250u32, &treasury, &100i128, &1_000_000i128);
@@ -455,11 +455,11 @@ mod test {
         assert!(!client.get_create_paused());
 
         let res = client.set_create_paused(&admin, &true);
-        assert!(res);
+        assert_eq!(res, ());
         assert!(client.get_create_paused());
 
         let res = client.set_create_paused(&admin, &false);
-        assert!(res);
+        assert_eq!(res, ());
         assert!(!client.get_create_paused());
     }
 
@@ -1381,7 +1381,7 @@ mod test {
 
         // Set emergency pause for 10 ledgers
         let res = client.set_emergency_pause(&admin, &true, &10u32);
-        assert!(res);
+        assert_eq!(res, ());
         assert!(client.get_create_paused());
 
         // Advance 9 ledgers - still paused
